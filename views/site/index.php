6e9e1cb6501188b5a770a7914388b857c3a0164c
Абашин Валерий <v.abashin@tcp.local>
2020-03-04 12:13:46 +0300

    auth

---
+++ views/site/index.php
@@ -0,0 +1,140 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->registerCssFile('/basic/web/css/site.css');
+
+?>
+
+<div class="content-wrapper">
+    <div class="left-panel">
+        <div class="numbers">
+            <div class="our_numbers">
+                <a class="numbers_our-numbers" href="#">Ваши номера</a>
+                <p class="numbers_numbers-mango">Номера MANGO OFFICE</p>
+                <p>7(495)660-83-15</p>
+                <p>7(495)660-83-15</p>
+                <p>7(495)660-83-15</p>
+            </div>
+            <div class="buy">
+                <a class="buy-button" href="#">Купить</a>
+                <div class="buy-wrapper"
+                <p>Схема <a href="#">Копия al2(1)</a></p>
+                <p>Схема <a href="#">Александр Кончиц</a></p>
+                <p>Схема <a href="#">dsh1-на sip,hard-2</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="numbers-buy">
+            <div class="num-buy">
+                <p class="numbers-buy_number_our_operator title">Номера других операторов</p>
+                <a class="item" href="#">Звонок с сайта №37</a>
+                <a class="item" href="#">123123123123123</a>
+                 <a class="item" href="#">Mysite.ru(12)</a>
+             </div>
+             <div class="connect">
+                    <a class="title" href="#">Подключить</a>
+                <div class="con-wrapper">
+                    <p class="item">Схема <a href="#">Новая схема line15088</a></p>
+                    <p class="item">Схема <a href="#">По умолчанию</a></p>
+                    <p class="item">Схема <a href="#">По умолчанию</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="widgets-wrapper">
+            <div class="widgets-site">
+                <p class="widgets-site_sites">Виджеты для сайта</p>
+                <a href="#">Test MCW</a>
+                <a href="#">PRO</a>
+                <a href="#">Mysite.ru</a>
+            </div>
+            <div class="add">
+                <a href="#">Добавить</a>
+                <a class="glyphicon glyphicon-home" href="#"></a>
+                <a class="glyphicon glyphicon-home" href="#"></a>
+                <a class="glyphicon glyphicon-home" href="#"></a>
+            </div>
+        </div>
+    </div>
+    <div class="left-panel">
+        <div class="numbers">
+            <div class="our_numbers">
+                <a class="numbers_our-numbers" href="#">Ваш персонал</a>
+                <p class="numbers_numbers-mango">Сотрудники</p>
+                <a><p>Pavel K</p></a>
+                <a><p>qqqqq</p></a>
+                <a><p>Андрей Гавриленко</p></a>
+            </div>
+            <div class="buy">
+                <p>внутренний номер</p>
+                <div class="buy-wrapper">
+                    <p>236</p>
+                    <p>3333</p>
+                    <p>235</p>
+                </div>
+            </div>
+        </div>
+        <div class="numbers-buy">
+            <div class="num-buy">
+                <p class="numbers-buy_number_our_operator title">Группы</p>
+                <a class="item" href="#">Новая группа</a>
+                <a class="item" href="#">ГруппаПро</a>
+                <a class="item" href="#">!!!!ARMANDELA</a>
+            </div>
+            <div class="connect">
+                <a class="title" href="#">распределение звонков</a>
+                <div class="con-wrapper">
+                    <p class="item">"Последовательный"</p>
+                    <p class="item">"Одновременно всем"</p>
+                    <p class="item">"Последовательный"</p>
+                </div>
+            </div>
+        </div>
+        <div class="numbers-buy">
+            <div class="num-buy">
+                <p class="numbers-buy_number_our_operator title">Интеграции</p>
+                <a class="item" href="#">API:Возможность использовать API-конструктор</a>
+                <a class="item" href="#">API:Интеграция с amoCRM</a>
+            </div>
+            <div class="connect">
+                <a class="title" href="#">Подключить</a>
+                <div class="service-wrapper">
+                    <p class="item">Услуга подключена</p>
+                    <p class="item">Услуга подключена</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer-wrapper">
+        <div class="call_processing">
+            <h4>Обработка звонков</h4>
+            <a href="#"><p>Номера подключенные к АТС</p></a>
+            <a href="#"><p>Голосовое меню и распределение звонков</p></a>
+            <a href="#"><p>Настройки ожидания ответа</p></a>
+            <a href="#"><p>Переадресация по номеру клиента</p></a>
+            <a href="#"><p>Черный и белый списки</p></a>
+            <a href="#"><p>Сотрудники и группы</p></a>
+        </div>
+        <div class="instruments">
+            <h4>Инструменты</h4>
+            <a class="" href="#"><p>Статистика и мониторинг</p></p></a>
+            <a href="#"><p>История вызовов</p></a>
+            <a href="#"><p>Запись разговоров</p></a>
+            <a href="#"><p>Сообщения и факсы</p></a>
+            <a href="#"><p>Заказ звонка</p></a>
+            <a href="#"><p>Статический коллтрекинг</p></a>
+            <a href="#"><p>Мультиканальный чат</p></a>
+         <a href="#"><p>Адресная книга</p></a>
+        </div>
+        <div class="ats_settings">
+            <h4>Настройки АТС</h4>
+            <a href="#"><p>Настройка SIP</p></a>
+            <a href="#"><p>Аудиофайлы</p></a>
+            <a href="#"><p>Безопасность и ограничения</p></a>
+            <a href="#"><p>Конференц-связь</p></a>
+            <a href="#"><p>Дополнительные настройки</p></a>
+            <a href="#"><p>Управления услугами</p></a>
+        </div>
+</div>
+
